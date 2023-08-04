94f8d95a74a941787a03dc1a716bec8eff1e26c9
ave6990 <ave6990@ya.ru>
2023-08-04 10:37:01 +0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   CHANGELOG.md
    	modified:   LICENSE
    	modified:   README.md
    	new file:   doc/intro.md
    	new file:   project.clj
    	new file:   src/metrology/.core.clj.swp
    	new file:   src/metrology/core.clj
    	new file:   src/metrology/data/auto.db
    	new file:   src/metrology/data/midb.db
    	new file:   src/metrology/data/personal.db
    	new file:   src/metrology/data/registry.db
    	new file:   src/metrology/data/tasks.db
    	new file:   src/metrology/lib/.chemistry.clj.swp
    	new file:   src/metrology/lib/chemistry.clj
    	new file:   test/metrology/core_test.clj

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to metrology
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/metrology/core.clj
@@ -0,0 +1,62 @@
+(ns metrology.core
+  (:gen-class)
+  (:require 
+    [clojure.math :refer :all]
+    [clojure.core :refer :all]
+    [clojure.repl :refer :all]
+    [clojure.java.jdbc :as jdbc]))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+
+(defn delta%
+  [value tolerance]
+  (let [dif (double (/ (* value tolerance) 100))]
+    (list (- value dif) (+ value dif))))
+
+(defn delta
+  [value tolerance]
+  (list (- value tolerance) (+ value tolerance)))
+
+(defn lel
+  [value component]
+  ())
+
+(comment
+
+(delta% 0.94 5)
+
+(map (fn [x] (* (/ x 4.4) 100)) '(0.216 0.663 0.769 1.077))
+
+(def lel (hash-map "ch4" 4.4 "c3h8" 1.7))
+
+(some (fn [x] (= x "c3h4")) (keys lel))
+
+(some (partial = "c3h4") (keys lel))
+
+(in-ns 'user)
+
+(in-ns 'metrology.metr)
+
+(re-seq #"[A-Z][a-z]?\d{0,2}" "NO2")
+
+(reduce + '(1 2 3 4))
+
+(clojure.string/upper-case "hello")
+
+;; documentation functions
+
+(apropos "upper")
+
+(doc clojure.string/upper-case)
+
+(find-doc "reduce")
+
+(source clojure.core/some)
+
+(dir clojure.core)
+
+)
