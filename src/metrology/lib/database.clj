e8bc6152fa3fab264f9589c6a4f3ebbd471a3488
ave6990 <ave6990@ya.ru>
2023-08-29 18:28:18 +0500

    On branch dev  Changes to be committed: 	modified:   src/metrology/lib/database.clj

--- src/metrology/lib/database.clj
+++ src/metrology/lib/database.clj
@@ -23,11 +23,35 @@
     ["select * from tasks
       where 
         (status >= 0)
-        and (status < 100);"]))
+        and (status < 100)
+      order by priority;"]))
+
+(defn miq
+  "SQL запросы к базе midb."
+  [& sql]
+  (jdbc/query
+    midb
+    (vec sql)))
 
 (comment
 
+(miq "Select * from verification limit 1;")
+
+(def ^:dynamic *x* 4)
+
+(list *x*)
+
+(binding [*x* 2])
+
+(assoc-in {:key1 1.2 :key2 32 :key3 200} [:key2] 4)
+
+(assoc {:key1 1.2 :key2 32 :key3 200} :key1 5.4)
+
 (active tasks)
+
+(require '[clojure.repl :refer :all])
+
+(find-doc "hash-map")
 
 (macroexpand '(defdb midb))
 
